045ccd0f8362f72c0edf3f911ab44588ef1485f8
aweell <aweell@users.noreply.github.com>
2022-06-02 22:33:20 +0000

    Update in library 2

---
+++ build/ios/library-2/tokens.h
@@ -0,0 +1,14 @@
+
+//
+// tokens.h
+//
+
+// Do not edit directly
+// Generated on Thu, 02 Jun 2022 22:33:20 GMT
+
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#define ColorLibrary2Purple [UIColor colorWithRed:0.388f green:0.271f blue:0.725f alpha:1.000f]
+
