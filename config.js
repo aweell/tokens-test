82f037006f5e15013e405c9a3e4c8bacdc07be03
Alex Bueno <44420072+aweell@users.noreply.github.com>
2022-06-02 22:34:05 +0200

    Change style dictionary config

---
+++ config.js
@@ -0,0 +1,41 @@
+const StyleDictionary = require('style-dictionary');
+
+module.exports = {
+  "source": [
+    "tokens/design-tokens-example.json",
+    
+  ],
+  "platforms": {
+    "css": {
+      "files": [
+        {
+          "destination": "variables.css",
+          "format": "css/variables"
+        }
+      ],
+      "transformGroup": "css",
+      "buildPath": "dist/css/"
+    },
+    "js": {
+      "transforms": ["attribute/cti", "name/cti/camel", "color/hex8"],
+      "files": [
+        {
+          "destination": "tokens.js",
+          "format": "javascript/es6"
+        }
+      ],
+      "transformGroup": "js",
+      "buildPath": "dist/js/"
+    },
+    "scss": {
+      "files": [
+        {
+          "destination": "variables.scss",
+          "format": "scss/variables"
+        }
+      ],
+      "transformGroup": "scss",
+      "buildPath": "dist/scss/"
+    }
+  }
+};
