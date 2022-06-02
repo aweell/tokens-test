6a90d92d5e752899256525ef4305581e9e9a3e1a
Alex Bueno <44420072+aweell@users.noreply.github.com>
2022-06-03 00:00:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,45 @@ const StyleDictionary = require('style-dictionary');
 module.exports = {
   "source": [
     "tokens/tokens-library-1.json",
+    
+  ],
+  "platforms": {
+    "css": {
+      "files": [
+        {
+          "destination": "variables.css",
+          "format": "css/variables"
+        }
+      ],
+      "transformGroup": "css",
+      "buildPath": "dist/library-1/css/"
+    },
+    "js": {
+      "transforms": ["attribute/cti", "name/cti/camel", "color/hex8"],
+      "files": [
+        {
+          "destination": "tokens.js",
+          "format": "javascript/es6"
+        }
+      ],
+      "transformGroup": "js",
+      "buildPath": "dist/library-1/js/"
+    },
+    "scss": {
+      "files": [
+        {
+          "destination": "variables.scss",
+          "format": "scss/variables"
+        }
+      ],
+      "transformGroup": "scss",
+      "buildPath": "dist/library-1/scss/"
+    }
+  }
+};
+
+module.exports = {
+  "source": [
     "tokens/tokens-library-2.json",
     
   ],
@@ -15,7 +54,7 @@ module.exports = {
         }
       ],
       "transformGroup": "css",
-      "buildPath": "dist/css/"
+      "buildPath": "dist/library-2/css/"
     },
     "js": {
       "transforms": ["attribute/cti", "name/cti/camel", "color/hex8"],
@@ -26,7 +65,7 @@ module.exports = {
         }
       ],
       "transformGroup": "js",
-      "buildPath": "dist/js/"
+      "buildPath": "dist/library-2/js/"
     },
     "scss": {
       "files": [
@@ -36,7 +75,8 @@ module.exports = {
         }
       ],
       "transformGroup": "scss",
-      "buildPath": "dist/scss/"
+      "buildPath": "dist/library-2/scss/"
     }
   }
 };
+
