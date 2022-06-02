a7bdbf0bde6acf60cbb50e27ccb617231b06639e
aweell <aweell@users.noreply.github.com>
2022-06-02 22:33:28 +0000

    Updated library 1

---
+++ build/ios/library-1/tokens.h
@@ -0,0 +1,14 @@
+
+//
+// tokens.h
+//
+
+// Do not edit directly
+// Generated on Thu, 02 Jun 2022 22:33:28 GMT
+
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#define ColorLibrary1Red [UIColor colorWithRed:1.000f green:0.000f blue:0.000f alpha:1.000f]
+
