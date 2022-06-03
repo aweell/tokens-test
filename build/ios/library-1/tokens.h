df19feb426da1966d521e09df1374ec97cc3243d
aweell <aweell@users.noreply.github.com>
2022-06-03 07:24:15 +0000

    Update in library 2

--- build/ios/library-1/tokens.h
+++ build/ios/library-1/tokens.h
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 02 Jun 2022 22:52:19 GMT
+// Generated on Fri, 03 Jun 2022 07:24:15 GMT
 
 
 #import <Foundation/Foundation.h>
--- build/ios/library-2/tokens.h
+++ build/ios/library-2/tokens.h
@@ -4,11 +4,20 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 02 Jun 2022 22:52:19 GMT
+// Generated on Fri, 03 Jun 2022 07:24:15 GMT
 
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
 #define ColorLibrary2Purple [UIColor colorWithRed:0.388f green:0.271f blue:0.725f alpha:1.000f]
+#define ColorColorRampLibrary2PurpleLibrary2Purple100 [UIColor colorWithRed:0.996f green:0.996f blue:0.996f alpha:1.000f]
+#define ColorColorRampLibrary2PurpleLibrary2Purple200 [UIColor colorWithRed:0.906f green:0.906f blue:0.933f alpha:1.000f]
+#define ColorColorRampLibrary2PurpleLibrary2Purple300 [UIColor colorWithRed:0.820f green:0.812f blue:0.875f alpha:1.000f]
+#define ColorColorRampLibrary2PurpleLibrary2Purple400 [UIColor colorWithRed:0.710f green:0.698f blue:0.812f alpha:1.000f]
+#define ColorColorRampLibrary2PurpleLibrary2Purple500 [UIColor colorWithRed:0.604f green:0.580f blue:0.765f alpha:1.000f]
+#define ColorColorRampLibrary2PurpleLibrary2Purple600 [UIColor colorWithRed:0.510f green:0.471f blue:0.729f alpha:1.000f]
+#define ColorColorRampLibrary2PurpleLibrary2Purple700 [UIColor colorWithRed:0.435f green:0.361f blue:0.718f alpha:1.000f]
+#define ColorColorRampLibrary2PurpleLibrary2Purple800 [UIColor colorWithRed:0.341f green:0.251f blue:0.616f alpha:1.000f]
+#define ColorColorRampLibrary2PurpleLibrary2Purple900 [UIColor colorWithRed:0.306f green:0.235f blue:0.537f alpha:1.000f]
 
