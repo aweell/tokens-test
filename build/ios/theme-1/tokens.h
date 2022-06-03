e91101bc58dbb9f84cac0dbf6effb57e39f28542
Alex Bueno <44420072+aweell@users.noreply.github.com>
2022-06-03 21:22:24 +0200

    Remove theme 2

---
+++ build/ios/theme-1/tokens.h
@@ -0,0 +1,15 @@
+
+//
+// tokens.h
+//
+
+// Do not edit directly
+// Generated on Fri, 03 Jun 2022 19:21:49 GMT
+
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#define ColorLibrary1Red [UIColor colorWithRed:1.000f green:0.000f blue:0.000f alpha:1.000f]
+#define ColorLibrary1Blue [UIColor colorWithRed:0.290f green:0.071f blue:0.922f alpha:1.000f]
+
