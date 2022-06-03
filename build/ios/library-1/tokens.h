0cfac2cac9c84c25cbc79c1630641bdc218385dc
aweell <aweell@users.noreply.github.com>
2022-06-03 13:45:30 +0000

    transform tokens

--- build/ios/library-1/tokens.h
+++ build/ios/library-1/tokens.h
@@ -4,11 +4,13 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 02 Jun 2022 22:52:19 GMT
+// Generated on Fri, 03 Jun 2022 13:45:29 GMT
 
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
 #define ColorLibrary1Red [UIColor colorWithRed:1.000f green:0.000f blue:0.000f alpha:1.000f]
+#define ColorLibrary1Blue [UIColor colorWithRed:0.290f green:0.071f blue:0.922f alpha:1.000f]
+#define ColorLibrary1Pink [UIColor colorWithRed:0.922f green:0.071f blue:0.580f alpha:1.000f]
 
--- build/ios/library-2/tokens.h
+++ build/ios/library-2/tokens.h
@@ -4,11 +4,21 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 02 Jun 2022 22:52:19 GMT
+// Generated on Fri, 03 Jun 2022 13:45:29 GMT
 
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
 #define ColorLibrary2Purple [UIColor colorWithRed:0.388f green:0.271f blue:0.725f alpha:1.000f]
+#define ColorTest01 [UIColor colorWithRed:1.000f green:0.933f blue:0.000f alpha:0.671f]
+#define ColorColorRampPurplePurple100 [UIColor colorWithRed:0.953f green:0.945f blue:0.980f alpha:1.000f]
+#define ColorColorRampPurplePurple200 [UIColor colorWithRed:0.898f green:0.878f blue:0.957f alpha:1.000f]
+#define ColorColorRampPurplePurple300 [UIColor colorWithRed:0.820f green:0.784f blue:0.922f alpha:1.000f]
+#define ColorColorRampPurplePurple400 [UIColor colorWithRed:0.729f green:0.678f blue:0.878f alpha:1.000f]
+#define ColorColorRampPurplePurple500 [UIColor colorWithRed:0.643f green:0.573f blue:0.839f alpha:1.000f]
+#define ColorColorRampPurplePurple600 [UIColor colorWithRed:0.557f green:0.471f blue:0.804f alpha:1.000f]
+#define ColorColorRampPurplePurple700 [UIColor colorWithRed:0.478f green:0.376f blue:0.769f alpha:1.000f]
+#define ColorColorRampPurplePurple800 [UIColor colorWithRed:0.408f green:0.290f blue:0.737f alpha:1.000f]
+#define ColorColorRampPurplePurple900 [UIColor colorWithRed:0.314f green:0.216f blue:0.584f alpha:1.000f]
 
