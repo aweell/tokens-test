c18a7cdb8ded273c974764a624c70baf0be2e630
Alex Bueno <44420072+aweell@users.noreply.github.com>
2022-06-04 08:46:44 +0200

    Add js export group

--- build/ios/theme-1/tokens.h
+++ build/ios/theme-1/tokens.h
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 03 Jun 2022 19:21:49 GMT
+// Generated on Sat, 04 Jun 2022 06:43:36 GMT
 
 
 #import <Foundation/Foundation.h>
---
+++ build/js/theme-1/tokens.js
@@ -0,0 +1,7 @@
+/**
+ * Do not edit directly
+ * Generated on Sat, 04 Jun 2022 06:43:36 GMT
+ */
+
+export const ColorLibrary1Red = "#ff0000";
+export const ColorLibrary1Blue = "#4a12eb";
