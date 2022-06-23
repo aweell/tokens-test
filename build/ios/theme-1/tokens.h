6f7754f79ebba1da56017f5e0cbbf1d9a2f0851d
Alex Bueno <44420072+aweell@users.noreply.github.com>
2022-06-23 17:18:17 +0200

    Add test with border-radius

--- build/ios/theme-1/tokens.h
+++ build/ios/theme-1/tokens.h
@@ -4,12 +4,13 @@
 //
 
 // Do not edit directly
-// Generated on Sat, 04 Jun 2022 06:30:58 GMT
+// Generated on Thu, 23 Jun 2022 15:13:32 GMT
 
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+#define BorderRadiusSmall 0.25rem
 #define ColorLibrary1Red [UIColor colorWithRed:1.000f green:0.000f blue:0.000f alpha:1.000f]
 #define ColorLibrary1Blue [UIColor colorWithRed:0.290f green:0.071f blue:0.922f alpha:1.000f]
 #define ColorLibrary1Pink [UIColor colorWithRed:0.922f green:0.071f blue:0.580f alpha:1.000f]
